9e077f412adf599808df6af89631d857632e0012
root <root@DESKTOP-9LK3UQP.localdomain>
2022-03-08 16:40:04 -0300

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {Usuario} from "./entity/Usuario";
 
 createConnection().then(async connection => {
 
     console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
+    const user = new Usuario();
+    user.nome = "Timber";
+    user.email = "timer@email.com";   
     await connection.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
 
     console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
+    const users = await connection.manager.find(Usuario);
     console.log("Loaded users: ", users);
 
     console.log("Here you can setup and run express/koa/any other framework.");
