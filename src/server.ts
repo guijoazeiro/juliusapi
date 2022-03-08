c004465b77f30f1ce84d2d20c7a21c77c1422415
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-08 17:36:40 -0300

    Add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
-import { conectarServidorNoBD } from './config/db'
+import {app} from './app'
 
-conectarServidorNoBD()
+
+const PORTA = 3000;
+
+const server = app.listen(PORTA, ()=>{
+    console.log(`App ouvindo a porta ${PORTA}`)
+})
+
+process.on('SIGINT', ()=>{
+    server.close();
+    console.log('\nApp finalizado')
+})
