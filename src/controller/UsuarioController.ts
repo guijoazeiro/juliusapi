9011f7944304554ef847c0508449ee025d3c4cc7
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-09 14:46:39 -0300

    Add save usuario controller

---
+++ src/controller/UsuarioController.ts
@@ -0,0 +1,9 @@
+import { getManager } from "typeorm";
+import { Usuario } from "../entity/Usuario";
+
+export class UsuarioController{
+    async salvar (usuario: Usuario){
+       const usuarioSalvo = await getManager().save(usuario)
+       return usuarioSalvo
+    }
+}
