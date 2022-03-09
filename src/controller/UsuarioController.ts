58d388af16a5790659a73f01c6438fa0404a8892
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-09 16:21:02 -0300

    Add recuperar lancamento

--- src/controller/UsuarioController.ts
+++ src/controller/UsuarioController.ts
@@ -16,4 +16,12 @@ export class UsuarioController {
         const usuario = await getManager().findOne(Usuario, id)
         return usuario
     }
+
+    async recuperarLancamentosDoUsuario(id: number) {
+        const usuario = await getManager()
+        .findOne(Usuario, id, {
+            relations: ['lancamentos']
+        });
+        return usuario.lancamentos;
+    }
 }
