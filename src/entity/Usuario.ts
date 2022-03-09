892195e3442dfc79245d5653e2c0c9c40a0bfb04
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-09 14:46:09 -0300

    Add usuario constructor

--- src/entity/Usuario.ts
+++ src/entity/Usuario.ts
@@ -1,7 +1,11 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class Usuario {
+    constructor(nome: string, email: string) {
+        this.nome = nome;
+        this.email = email
+    }
 
     @PrimaryGeneratedColumn()
     id: number;
@@ -11,5 +15,5 @@ export class Usuario {
 
     @Column()
     email: string;
-  
+
 }
