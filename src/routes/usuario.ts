80992cdf530e97b75c44ba5745da5995c27a4e59
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-09 14:46:59 -0300

    Add usuario router

---
+++ src/routes/usuario.ts
@@ -0,0 +1,13 @@
+import { Request, Response, Router } from "express";
+import { UsuarioController } from "../controller/UsuarioController";
+import { Usuario } from "../entity/Usuario";
+
+export const routerUsuario = Router()
+const usuarioCtrl = new UsuarioController()
+
+routerUsuario.post('/', async (req: Request, res: Response) => {
+    const {nome, email} = req.body;
+    const usuario = new Usuario(nome, email)
+    const usuarioSalvo = await usuarioCtrl.salvar(usuario)    
+    res.status(200).json(usuarioSalvo)
+})
